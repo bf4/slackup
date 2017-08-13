5bb8cc42009018c766e14863191d3961155355bd
Benjamin Fleischer <github@benjaminfleischer.com>
2017-08-12 22:10:54 -0500

    Extract users

--- lib/slackup.rb
+++ lib/slackup.rb
@@ -5,6 +5,7 @@ require "yaml"
 require "fileutils"
 gem "slack-api", "~> 1.1", ">= 1.1.3"
 require "slack"
+require_relative 'slackup/users'
 require_relative 'slackup/channels'
 require_relative 'slackup/groups'
 require_relative 'slackup/ims'
@@ -50,7 +51,7 @@ class Slackup
           Channels.new(name, @token).write!
           Groups.new(name, @token).write!
           Stars.new(name, @token).write!
-          write_users
+          user_client.write!
           Ims.new(name, @token).write!
         end
     end
@@ -70,59 +71,6 @@ class Slackup
       p [name, auth_test]
       false
     end
-  end
-
-  User = Struct.new(:user_hash) do
-    def id; user_hash["id"]; end
-
-    def name; user_hash["name"]; end
-
-    def deleted; user_hash["deleted"]; end
-
-    def color; user_hash["color"]; end
-
-    def profile; user_hash["profile"]; end
-
-    def admin?; user_hash["is_admin"]; end
-
-    def owner?; user_hash["is_owner"]; end
-
-    def has_2fa?; user_hash["has_2fa"]; end
-
-    def has_files?; user_hash["has_files"]; end
-
-    def to_hash; user_hash; end
-  end
-  # {
-  #   "ok": true,
-  #   "members": [
-  #     {
-  #       "id": "U023BECGF",
-  #       "name": "bobby",
-  #       "deleted": false,
-  #       "color": "9f69e7",
-  #       "profile": {
-  #         "first_name": "Bobby",
-  #         "last_name": "Tables",
-  #         "real_name": "Bobby Tables",
-  #         "email": "bobby@slack.com",
-  #         "skype": "my-skype-name",
-  #         "phone": "+1 (123) 456 7890",
-  #         "image_24": "https:\/\/...",
-  #         "image_32": "https:\/\/...",
-  #         "image_48": "https:\/\/...",
-  #         "image_72": "https:\/\/...",
-  #         "image_192": "https:\/\/..."
-  #       },
-  #       "is_admin": true,
-  #       "is_owner": true,
-  #       "has_2fa": false,
-  #       "has_files": true
-  #     },
-  #   ]
-  # }
-  def users
-    @users ||= Slack.users_list["members"].map { |member| User.new(member) }
   end
 
 
@@ -157,20 +105,16 @@ class Slackup
     }.compact
   end
 
-  # gets user name for an id, if mapping is known, else returns the input
-  def user_name(user_id)
-    @user_names ||= users.each_with_object({}) {|user, lookup|
-      lookup[user.id] = user.name
-    }
-    @user_names.fetch(user_id) {
-      user_id
-    }
+  def user_client
+    @user_client ||= Users.new(name, @token)
   end
 
-  def write_users
-    File.open(backup_filename("users"), "w")  do |f|
-      f.write(serialize(users.map(&:to_hash)))
-    end
+  def users
+    user_client.users
+  end
+
+  def user_name(user_id)
+    user_client.user_name(user_id)
   end
 
   def serialize(obj)
---
+++ lib/slackup/users.rb
@@ -0,0 +1,71 @@
+class Slackup::Users < Slackup
+  User = Struct.new(:user_hash) do
+    def id; user_hash["id"]; end
+
+    def name; user_hash["name"]; end
+
+    def deleted; user_hash["deleted"]; end
+
+    def color; user_hash["color"]; end
+
+    def profile; user_hash["profile"]; end
+
+    def admin?; user_hash["is_admin"]; end
+
+    def owner?; user_hash["is_owner"]; end
+
+    def has_2fa?; user_hash["has_2fa"]; end
+
+    def has_files?; user_hash["has_files"]; end
+
+    def to_hash; user_hash; end
+  end
+  # {
+  #   "ok": true,
+  #   "members": [
+  #     {
+  #       "id": "U023BECGF",
+  #       "name": "bobby",
+  #       "deleted": false,
+  #       "color": "9f69e7",
+  #       "profile": {
+  #         "first_name": "Bobby",
+  #         "last_name": "Tables",
+  #         "real_name": "Bobby Tables",
+  #         "email": "bobby@slack.com",
+  #         "skype": "my-skype-name",
+  #         "phone": "+1 (123) 456 7890",
+  #         "image_24": "https:\/\/...",
+  #         "image_32": "https:\/\/...",
+  #         "image_48": "https:\/\/...",
+  #         "image_72": "https:\/\/...",
+  #         "image_192": "https:\/\/..."
+  #       },
+  #       "is_admin": true,
+  #       "is_owner": true,
+  #       "has_2fa": false,
+  #       "has_files": true
+  #     },
+  #   ]
+  # }
+  def list
+    @list ||= Slack.users_list["members"].map { |member| User.new(member) }
+  end
+  alias users list
+
+  def write!
+    File.open(backup_filename("users"), "w")  do |f|
+      f.write(serialize(users.map(&:to_hash)))
+    end
+  end
+
+  # gets user name for an id, if mapping is known, else returns the input
+  def user_name(user_id)
+    @user_names ||= users.each_with_object({}) {|user, lookup|
+      lookup[user.id] = user.name
+    }
+    @user_names.fetch(user_id) {
+      user_id
+    }
+  end
+end
